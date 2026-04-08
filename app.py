5d89ffb28fed44f541f1c4396837e3f0bf89bf4d
Brütsch Sebastian (bruetseb) <bruetseb@students.zhaw.ch>
2026-04-08 02:11:31 +0200

    fix

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ st.markdown("""
         color: white;
     }
     </style>
-    """, unsafe_base_hint=True)
+    """, unsafe_allow_html=True)
 
 st.title("🇨🇭 Swiss German Generation Predictor")
 st.markdown("""
